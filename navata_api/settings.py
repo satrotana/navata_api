bf1890c4c00b8b6918e22b8fa08739d0d415ca53
satrotana <satrotana@gmail.com>
2022-09-15 20:27:54 +0700

    setting

--- navata_api/settings.py
+++ navata_api/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-import dj_database_url
+import os
+import django_heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +108,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
